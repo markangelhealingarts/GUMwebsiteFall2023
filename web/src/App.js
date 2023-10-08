19e9e0bb9243bebad278213805dbcec015743254
Georgekorolev <Mr.rookie123@gmail.com>
2023-10-07 20:10:48 -0700

    Update app.js to use layout component

--- web/src/App.js
+++ web/src/App.js
@@ -1,18 +1,18 @@
 // src/App.js
 import React from 'react';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Layout from './components/Layout';
+import Homepage from './components/Hompage_content';
 import './App.css'; // Consider moving your main CSS here
 import 'tailwindcss/tailwind.css';
 
 function App() {
-    return (
-        <div className="App">
-            <Navbar />
-            <Footer />
-            {/* Any Modals like "read-more" and "disclaimer" can be components too and included here */}
-        </div>
-    );
+  return (
+    <div className="App">
+      <Layout>
+        <Homepage />
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
