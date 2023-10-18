cf95ec2a6f929754e5af23319a543e3c96d4b797
Georgekorolev <Mr.rookie123@gmail.com>
2023-10-18 11:40:32 -0700

    Implement routing in App.js

--- web/src/App.js
+++ web/src/App.js
@@ -2,16 +2,23 @@
 import React from 'react';
 import Layout from './components/Layout';
 import Homepage from './components/Hompage_content';
+import StartMoving from './components/StartMoving';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'; // Consider moving your main CSS here
 import 'tailwindcss/tailwind.css';
 
 function App() {
   return (
-    <div className="App">
-      <Layout>
-        <Homepage />
-      </Layout>
-    </div>
+    <Router>
+      <div className="App">
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/start_moving" element={<StartMoving />} />
+          </Routes>
+        </Layout>
+      </div>
+    </Router>
   );
 }
 
