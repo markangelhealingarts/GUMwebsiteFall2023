5cc2576fb3d49255c18f948e7a11525b7d2cc046
Georgui <gkorolev@csumb.edu>
2023-11-05 15:19:43 -0800

    Update color of button component

--- web/src/components/Button.js
+++ web/src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button = ({ label, onClick }) => {
   return (
-    <button onClick={onClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full m-1">
+    <button onClick={onClick} className="bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-lg m-1">
       {label}
     </button>
   );
