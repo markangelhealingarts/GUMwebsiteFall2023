f1b36ebbbda1af69f84bbe3d8991c6c019673fd2
Georgui <gkorolev@csumb.edu>
2023-11-29 11:46:49 -0800

    Update currentTimer to useRef()

--- web/src/AuthContext.js
+++ web/src/AuthContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useEffect, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState, useRef } from 'react';
 import { auth, db } from './firebase';
 import { doc, getDoc } from 'firebase/firestore';
 import Timer from './Timer';
@@ -11,10 +11,10 @@ export const useAuth = () => useContext(AuthContext);
 export const AuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
   const navigate = useNavigate();
+  let currentTimer = useRef(null);
   const [currentTimerSession, setCurrentTimerSession] = useState(0);
 
   useEffect(() => {
-    let currentTimer = null;
     const unsubscribe = auth.onAuthStateChanged(async (user) => {
       setCurrentUser(user);
       if (user) {
@@ -25,19 +25,24 @@ export const AuthProvider = ({ children }) => {
           const userLevel = userData.level;
           const nextTimerTime = userData.timers[currentTimerSession];
           if (nextTimerTime) {
-            currentTimer = new Timer(nextTimerTime, () => {
+            currentTimer.current = new Timer(nextTimerTime, () => {
               console.log("Timer expired!")
               setCurrentTimerSession((prev) => (prev >= 5) ?  0 : prev + 1); // Update the session for the next timer
               alert(`Hey ${userData.username}, it's time to Get Up and Move!`);
               navigate(`/video/${userLevel}/${currentTimerSession + 1}`);
             });
-            currentTimer.start();
+            currentTimer.current.start();
           }
         }
       }
     });
 
-    return () => unsubscribe();
+    return () => {
+      unsubscribe();
+      if (currentTimer.current) {
+        currentTimer.current.stop();
+      }
+    };
   }, [currentTimerSession, navigate]);
 
   return (
