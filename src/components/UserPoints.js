62edee2e6b979f0a6976794838310096f0be28b1
Georgui <gkorolev@csumb.edu>
2023-12-06 10:51:16 -0800

    Update profile page to display points even on page reload

--- src/components/UserPoints.js
+++ src/components/UserPoints.js
@@ -1,30 +1,41 @@
-import React, { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { auth, db } from '../firebase';
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc } from 'firebase/firestore';
 
 const UserPoints = () => {
   const [userData, setUserData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const fetchUserData = async () => {
-      const currentUser = auth.currentUser;
+    const unsubscribe = auth.onAuthStateChanged(async (currentUser) => {
       if (currentUser) {
-        const userRef = doc(db, "Users", currentUser.uid);
-        const userDoc = await getDoc(userRef);
-        if (userDoc.exists()) {
-          setUserData(userDoc.data());
-        } else {
-          // Handle case where document doesn't exist
+        const userRef = doc(db, 'Users', currentUser.uid);
+        try {
+          const userDoc = await getDoc(userRef);
+          if (userDoc.exists()) {
+            setUserData(userDoc.data());
+          } else {
+            console.error("User document does not exist");
+          }
+        } catch (error) {
+          console.error("Error fetching user data:", error);
         }
+      } else {
+        console.log('No current user.');
       }
-    };
+      setIsLoading(false);
+    });
 
-    fetchUserData();
+    return () => unsubscribe(); // Cleanup subscription on unmount
   }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className='p-2'>
-      <h1>Your Points: {userData ? userData.points : 0}</h1>
+      <h1>Your Points: {userData ? userData.points : 'Not available'}</h1>
     </div>
   );
 };
